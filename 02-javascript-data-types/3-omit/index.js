3c2cc1ed4dc8319288513a5445f0af19e51a6c72
alexander <foo@bar>
2021-01-24 00:35:40 +0300

    solution 2-3 omit

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,11 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+  let res = {};
+  for (let entry of Object.entries(obj)) {
+    if (!fields.includes(entry[0])) {
+      res[entry[0]] = entry[1];
+    }
+  }
+  return res;
 };
